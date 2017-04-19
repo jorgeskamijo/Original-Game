02521e067fd71d379686be2665146cd4c4ef5bb3
jorgeskamijo <reincarnation404@yahoo.co.jp>
2017-04-19 16:47:22 +0900

    originalgame　基本
    ジャンプ追加

--- Assets/uchinoko/Player.cs
+++ Assets/uchinoko/Player.cs
@@ -5,9 +5,12 @@ public class Player : MonoBehaviour
 {
 
     public float speed = 4f; //歩くスピード
+    public float jumpPower = 700; //ジャンプ力
+    public LayerMask groundLayer; //Linecastで判定するLayer
     public GameObject mainCamera;
     private Rigidbody2D rigidbody2D;
     private Animator anim;
+    private bool isGrounded; //着地判定
 
     void Start()
     {
@@ -16,15 +19,49 @@ public class Player : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        //Linecastでユニティちゃんの足元に地面があるか判定
+        isGrounded = Physics2D.Linecast(
+        transform.position + transform.up * 1,
+        transform.position - transform.up * 1.1f,
+        groundLayer);
+        //スペースキーを押し、
+        if (Input.GetKeyDown("space"))
+        {
+            //着地していた時、
+            if (isGrounded)
+            {
+                //Dashアニメーションを止めて、Jumpアニメーションを実行
+                anim.SetBool("Dash", false);
+                anim.SetTrigger("Jump");
+                //着地判定をfalse
+                isGrounded = false;
+                //AddForceにて上方向へ力を加える
+                rigidbody2D.AddForce(Vector2.up * jumpPower);
+            }
+        }
+        //上下への移動速度を取得
+        float velY = rigidbody2D.velocity.y;
+        //移動速度が0.1より大きければ上昇
+        bool isJumping = velY > 0.1f ? true : false;
+        //移動速度が-0.1より小さければ下降
+        bool isFalling = velY < -0.2f ? true : false;
+        //結果をアニメータービューの変数へ反映する
+        anim.SetBool("isJumping", isJumping);
+        anim.SetBool("isFalling", isFalling);
+    }
+
     void FixedUpdate()
     {
         //左キー: -1、右キー: 1
-        float x = Input.GetAxisRaw("Horizontal");
+        float scale = 2.5f; //オブジェクトのサイズ
+        float x = Input.GetAxisRaw("Horizontal")　*　scale;
         //左か右を入力したら
         if (x != 0)
         {
             //入力方向へ移動
-            rigidbody2D.velocity = new Vector2(x * speed, rigidbody2D.velocity.y);
+            rigidbody2D.velocity = new Vector2(x　/3 * speed, rigidbody2D.velocity.y);
             //localScale.xを-1にすると画像が反転する
             Vector2 temp = transform.localScale;
             temp.x = x;
